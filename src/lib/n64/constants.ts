535e59181ba3c0e44af395548eef9fff05a973cf
D3Portillo <d3portillo@gmail.com>
2024-03-23 05:56:19 +0100

    FEAT: add n64wasm assets + constants

---
+++ src/lib/n64/constants.ts
@@ -0,0 +1,11 @@
+export const SETTINGS = {
+  CLOUDSAVEURL: "",
+  SHOWADVANCED: false, // Enable importing of save files (eep, sra, fla)
+  SHOWOPTIONS: false, // Reset and toggling fps
+}
+
+export const ROM_LIST = [
+  /*
+    {url:"roms/rom1.z64",title:"Game 1"},
+    */
+]
